56d1a8892b8945a6ff4f54eb711ab8b6b903a155
Plamen Stoev <psstoev@gmail.com>
2015-11-30 22:56:34 +0200

    Ex. 1.10, 1.11

---
+++ ch1/exercises/ex10/run.bat
@@ -0,0 +1,4 @@
+@echo off
+go run main.go https://en.wikipedia.org/wiki/List_of_law_clerks_of_the_Supreme_Court_of_the_United_States
+go run main.go https://en.wikipedia.org/wiki/List_of_law_clerks_of_the_Supreme_Court_of_the_United_States
+@echo on
---
+++ ch1/exercises/ex11/run.bat
@@ -0,0 +1,4 @@
+@echo off
+REM TODO: look for a slow, but responding web site
+go run main.go https://google.com https://facebook.com https://asdfsadfasdfsadf.com https://en.wikipedia.org
+@echo on
